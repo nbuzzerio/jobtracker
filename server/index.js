e3360129ce0042624581442ae90a10a19f0c2e7a
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-23 16:43:11 -0400

    initial app setup

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname + '/../client/dist')))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port: ${port}`));
