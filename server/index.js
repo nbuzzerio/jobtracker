e3527c1c5baf28a8ecddcb2e69f5d73a8f3947e2
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-24 18:38:42 -0400

    created post route to create new user. used bcrypt to salt and hash user passwords

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const user = require('../routes/user');
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-        .then(() => console.log('Connected to MongoDB...'));
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(() => console.log('Connected to MongoDB...'));
 
-app.use('/api/users', user)
+app.use(express.json());
+app.use('/api/users', user);
 
-app.use(express.static(path.join(__dirname + '/../client/dist')))
+app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
